bde2007b310cdacf41b335643141f0b197cd61f2
Deep Lalwani <deepthesmartest@gmail.com>
2025-05-07 00:00:03 +0530

    Fixed No entries screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,14 +33,15 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.viewpager2:viewpager2:1.0.0")
-    implementation("androidx.navigation:navigation-fragment:2.7.6")
-    implementation("androidx.navigation:navigation-ui:2.7.6")
-    implementation("androidx.annotation:annotation:1.7.1")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.viewpager2:viewpager2:1.1.0")
+    implementation("androidx.navigation:navigation-fragment:2.8.9")
+    implementation("androidx.navigation:navigation-ui:2.8.9")
+    implementation("androidx.annotation:annotation:1.9.1")
     implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
--- app/src/main/java/com/example/expensetracker/fragments/UnconfirmedEntriesFragment.java
+++ app/src/main/java/com/example/expensetracker/fragments/UnconfirmedEntriesFragment.java
@@ -1,13 +1,13 @@
 package com.example.expensetracker.fragments;
 
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.appcompat.app.AlertDialog;
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,6 +16,9 @@ import com.example.expensetracker.MainActivity;
 import com.example.expensetracker.R;
 import com.example.expensetracker.adapters.UnconfirmedEntriesAdapter;
 import com.example.expensetracker.database.DBManager;
+import com.example.expensetracker.pojo.UnconfirmedEntry;
+
+import java.util.ArrayList;
 
 public class UnconfirmedEntriesFragment extends Fragment {
     @Override
@@ -29,10 +32,15 @@ public class UnconfirmedEntriesFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_unconfirmed_entries, container, false);
 
         RecyclerView recycler = view.findViewById(R.id.unconfirmed_recycleView);
-        recycler.setAdapter(new UnconfirmedEntriesAdapter(DBManager.getDBManagerInstance().getUnconfirmedEntries()));
+        ArrayList<UnconfirmedEntry> list = DBManager.getDBManagerInstance().getUnconfirmedEntries();
+        recycler.setAdapter(new UnconfirmedEntriesAdapter(list));
 
         recycler.setLayoutManager(new LinearLayoutManager(requireActivity()));
         recycler.setVerticalScrollbarPosition(0);
+        if(list.size() == 0) {
+            recycler.setVisibility(View.GONE);
+            view.findViewById(R.id.text_no_entries).setVisibility(View.VISIBLE);
+        }
 
         Button button = view.findViewById(R.id.discard_all_btn);
         button.setOnClickListener(v -> {
